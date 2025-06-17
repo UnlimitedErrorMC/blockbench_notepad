1365ea2e2dcb2864217036d5334e79ed6b1dcd41
UnlimitedFatalError <bowserbiryani@hotmail.com>
2025-06-18 01:53:52 +0300

    Upload initial files

---
+++ blockbench_notepad.js
@@ -0,0 +1,10 @@
+Plugin.register('blockbench_notepad', {
+    title: 'Blockbench Notepad',
+    author: 'UnlimitedError',
+    icon: 'icon',
+    description: '',
+    version: '1.0.0',
+    variant: 'both',
+    onload() {
+    }
+});
